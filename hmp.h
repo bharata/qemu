7c80d8838f3c7c6451acb150f5c799618d1c34dc
Bharata B Rao <bharata@linux.vnet.ibm.com>
2015-04-30 12:32:23 +0530

    hmp: Add "info spapr-cpu-sockets" command
    This is the hmp equivalent of "query spapr-cpu-sockets"
    
    Signed-off-by: Bharata B Rao <bharata@linux.vnet.ibm.com>

--- hmp.h
+++ hmp.h
@@ -95,6 +95,7 @@ void hmp_object_add(Monitor *mon, const QDict *qdict);
 void hmp_object_del(Monitor *mon, const QDict *qdict);
 void hmp_info_memdev(Monitor *mon, const QDict *qdict);
 void hmp_info_memory_devices(Monitor *mon, const QDict *qdict);
+void hmp_info_spapr_cpu_sockets(Monitor *mon, const QDict *qdict);
 void object_add_completion(ReadLineState *rs, int nb_args, const char *str);
 void object_del_completion(ReadLineState *rs, int nb_args, const char *str);
 void device_add_completion(ReadLineState *rs, int nb_args, const char *str);
